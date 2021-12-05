5f977cb2fb64656d8e5eb4547b1600b39c805b2f
vscoding1988 <vscoding1988@gmail.com>
2021-12-05 18:42:05 +0100

    cleanup

--- ops/azure/terraform/aks-helm/helm_release_nginx.tf
+++
@@ -1,7 +0,0 @@
-# this will download the helm chart from nginx repo
-resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress"
-  repository = "https://helm.nginx.com/stable"
-  chart      = "nginx-ingress"
-  wait       = "true"
-}
