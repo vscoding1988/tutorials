4f47cf7582cc1493d3546ff4eab425144a69312d
vscoding1988 <vscoding1988@gmail.com>
2023-02-15 06:49:33 +0100

    version up, added lombok

--- spring-boot/spring-boot-jpa/src/main/java/com/vscoding/jpa/entity/ProductEntity.java
+++ spring-boot/spring-boot-jpa/src/main/java/com/vscoding/jpa/entity/ProductEntity.java
@@ -4,7 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class ProductEntity {
 
@@ -15,25 +19,5 @@ public class ProductEntity {
   private String name;
 
   public ProductEntity() {
-  }
-
-  public ProductEntity(String name) {
-    this.name = name;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 }
