9e3243dce334e6a5a6517e7adcfc5db791378265
vscoding1988 <vscoding1988@gmail.com>
2023-03-03 07:49:56 +0100

    update base to spring boot 3.0.3

---
+++ spring-boot/spring-boot-base/src/test/java/com/vscoding/tutorial/spring/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.vscoding.tutorial.spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+  @Test
+  void contextUp() {
+    assertTrue(true);
+  }
+}
