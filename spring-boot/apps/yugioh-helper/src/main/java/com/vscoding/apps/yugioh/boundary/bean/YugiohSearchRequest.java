3ca71a7ac667a74d55f81eda3dada4fe8b1c9fe5
vscoding <vscoding1988@gmail.com>
2026-02-16 19:24:42 +0100

    add search pagination

--- spring-boot/apps/yugioh-helper/src/main/java/com/vscoding/apps/yugioh/boundary/bean/YugiohSearchRequest.java
+++ spring-boot/apps/yugioh-helper/src/main/java/com/vscoding/apps/yugioh/boundary/bean/YugiohSearchRequest.java
@@ -1,4 +1,12 @@
 package com.vscoding.apps.yugioh.boundary.bean;
 
-public record YugiohSearchRequest(String query) {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class YugiohSearchRequest {
+  private String query;
+  private int page = 0;
+  private int limit = 20;
 }
--- spring-boot/apps/yugioh-helper/src/main/java/com/vscoding/apps/yugioh/control/YugiohSearchService.java
+++ spring-boot/apps/yugioh-helper/src/main/java/com/vscoding/apps/yugioh/control/YugiohSearchService.java
@@ -4,7 +4,7 @@ import com.vscoding.apps.yugioh.boundary.bean.YugiohSearchRequest;
 import com.vscoding.apps.yugioh.boundary.bean.YugiohSearchResponse;
 import com.vscoding.apps.yugioh.entity.YugiohDataCardRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +14,9 @@ public class YugiohSearchService {
   private final YugiohMapper mapper = new YugiohMapper();
 
   public YugiohSearchResponse search(YugiohSearchRequest request) {
-    var pageable = Pageable.ofSize(25);
+    var pageable = PageRequest.of(request.getPage(), request.getLimit());
 
-    var result = repository.findAllByNameContainingIgnoreCaseOrNameEnContainingIgnoreCase(request.query(), request.query(), pageable);
+    var result = repository.findAllByNameContainingIgnoreCaseOrNameEnContainingIgnoreCase(request.getQuery(), request.getQuery(), pageable);
     var cards = result.get().map(mapper::mapLazy).toList();
 
     return new YugiohSearchResponse(result.getTotalElements(), cards);
